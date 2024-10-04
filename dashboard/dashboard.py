0b0ed82f02c879842075de9973273083c9693dac
Muhammad Arsayuan Wijaya <arsayuans@gmail.com>
2024-10-04 23:56:10 +0700

    Update dashboard.py
    menambahkan import ampy

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import ampy
 
 # Set judul untuk dashboard
 st.title("Dashboard Layanan Penyewaan Sepeda")
